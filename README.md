9121a08480b377ca507e3b3af9eefa7abb9ad445
ray-seriously <65574297+ray-seriously@users.noreply.github.com>
2020-07-24 03:11:09 -0400

    I stayed up until 3 am/mistakes were made

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CodeLou
+# Fake fortune teller website:
+# This is a goofy project meant to make me laugh because I'm a big kid. The complaints form does not submit, on purpose, because I want to mess with the user. The fortunes are also less-than-useful.
+# Feature requirements:
+# Mobile responsive navigation menu
+# Form that takes in at elast two fields of information and validates input
+# JavaScropt function whose return value is used in your site
+# Use ":nth-child" CSS to style a series of elements
--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const toggle = document.querySelector(".toggle");
-const topnav = document.querySelector(".topnav");
- 
-/* Toggle mobile menu */
-function toggleNav() {
-    if (topnav.classList.contains("active")) {
-        topnav.classList.remove("active");
-         
-        // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<img src="images/crystal_ball.png">";
-    } else {
-        topnav.classList.add("active");
-         
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<img src="images/crystal_ball.png">";
-    }
-}
- 
-/* Event Listener */
-toggle.addEventListener("click", toggleNav, false);
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
 
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
