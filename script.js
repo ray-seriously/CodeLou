e3a50d2d8d9c89ed276eec43148904e4b9f97a73
ray-seriously <65574297+ray-seriously@users.noreply.github.com>
2020-07-24 23:53:38 -0400

    mostly finished css and js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Hamburger menu //
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 });
+
+// Complaints page respone alert //
+function complaintAlert() {
+    alert("Due to unforseen circumstances, your complaint cannot be submitted at this time. To file a complaint, please visit our Complaints page.");
+  }
